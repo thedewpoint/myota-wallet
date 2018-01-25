0b4c9e5cee9f1f4c9bc55f5605954e6631e2c40e
DJ <dj.bowden7@gmail.com>
2018-01-24 23:52:54 -0500

    added blockie and random seed generator ui

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,7 @@ import {Iota} from 'iota-basic';
 import {INode, NodeService} from './services/node.service';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/filter';
+import * as blockies from 'ethereum-blockies';
 
 // import {DialogComponent} from './dialog/dialog.component';
 
@@ -18,8 +19,9 @@ export class AppComponent {
   checksum:string;
   nodeService:NodeService;
   nodes: INode[];
+  blockie: string;
 
-  constructor(nodeService: NodeService) {
+  constructor(nodeService: NodeService, domSanitizer: DomSanitizer) {
     this.nodeService = nodeService;
   }
   ngOnInit(): void {
@@ -32,7 +34,25 @@ export class AppComponent {
   }
   update(seed){
     const iota = new Iota(seed);
+    this.seed = seed;
     this.checksum = iota.getChecksum();
+    const canvas = blockies.create({ // All options are optional 
+      seed: seed, // seed used to generate icon data, default: random 
+      color: '#03a9f4', // to manually specify the icon color, default: random 
+      bgcolor: '#303030', // choose a different background color, default: random 
+      size: 15, // width/height of the icon in blocks, default: 8 
+      scale: 3, // width/height of each block in pixels, default: 4 
+      spotcolor: '#FFFFFF' // each pixel has a 13% chance of being of a third color,  
+      // default: random. Set to -1 to disable it. These "spots" create structures 
+      // that look like eyes, mouths and noses.  
+  });
+  this.blockie = canvas.toDataURL();
   }
 
+  generateSeed() {
+    const iota = new Iota('');
+   iota.generateSeed().then((seed)=>{
+    this.update(seed);
+   });
+  }
 }
--- src/app/services/node.service.ts
+++ src/app/services/node.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
-
+//http://motyar.info/webscrapemaster/api/?url=http://testing-ground.scraping.pro/blocks&xpath=//div[@id=case1]/div[1]/span[1]/div
 export interface INode {
   node: string;
   port: string;
